7d088890a45ac457669e6d552ddba47ed9bd1b57
kevwad <kevin.wadera.ca@gmail.com>
2025-04-21 16:13:01 -0400

    Initial Commit

---
+++ RedditTopPostsTests/RedditTopPostsTests.swift
@@ -0,0 +1,17 @@
+//
+//  RedditTopPostsTests.swift
+//  RedditTopPostsTests
+//
+//  Created by Kevin Wadera on 2025-04-21.
+//
+
+import Testing
+@testable import RedditTopPosts
+
+struct RedditTopPostsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
